f4d65b5c6de0df761f18aa7cc9ee89fd64f658ae
Rodevp <rodriguezrodrigor12@gmail.com>
2022-03-08 10:37:05 -0500

    creando primer controlador

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const express = require('express')
 const app = express()
 const route = require('express').Router()
 
+//controllers
+const {
+    getAllNotes
+} = require('./controller')
+
+app.use( express.json() )
 
 route.get('/',  (req, res) => {
-    res.json({
-        message: 'Server on port 3000'
-    })
+    const notes = getAllNotes()
+
+    if(notes.length === 0) {
+        return res.json( { message: 'No hay notas' } ).status(204)
+    }
+    
+    return res.json( { message: 'ok', notes } ).status(200)
+
 })
 
 app.use(route)
---
+++ controller.js
@@ -0,0 +1,35 @@
+const data = [];
+
+const getAllNotes = () => {
+    return data
+}
+
+const getNote = () => {
+
+}
+
+const addNote = () => {
+
+}
+
+const deleteNote = () => {
+
+}
+
+const updateNote = () => {
+
+}
+
+const createNote = () => {
+
+}
+
+
+module.exports = {
+    addNote,
+    deleteNote,
+    updateNote,
+    createNote,
+    getAllNotes,
+    getNote
+}
